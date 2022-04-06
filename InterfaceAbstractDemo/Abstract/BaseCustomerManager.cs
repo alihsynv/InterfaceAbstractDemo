03637aea1beca539358853434674ade4e253c7bb
alihsynv <alihsynov@gmail.com>
2022-04-06 16:08:25 +0300

    Add project files.

---
+++ InterfaceAbstractDemo/Abstract/BaseCustomerManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InterfaceAbstractDemo.Entities;
+
+namespace InterfaceAbstractDemo.Abstract
+{
+    public abstract class BaseCustomerManager:ICustomerCheckService
+    {
+        public void CheckIfRealPerson(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Save(Customer customer)
+        {
+            Console.WriteLine("Saved to db :" + customer.FirstName);
+        }
+
+        bool ICustomerCheckService.CheckIfRealPerson(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Adapters;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
+            customerManager.Save(new Customer
+            {
+                DateOfBirth = new DateTime(2000, 3, 17), FirstName = "Ali", LastName = "Huseynov",
+                NationalityId = "12312312312"
+            });
+            Console.ReadLine();
+        }
+    }
+}
